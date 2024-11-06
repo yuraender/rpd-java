6c37a455a41bbc0d5238467e0b7a598477d18fff
YuraEnder <yuraender@yandex.ru>
2024-11-06 21:52:12 +0300

    Update project

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("mysql:mysql-connector-java:5.1.49")
+	implementation("javax.servlet:javax.servlet-api:4.0.1")
+	implementation("org.apache.poi:poi-ooxml:5.2.3")
+	implementation("commons-io:commons-io:2.11.0")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
--- src/main/java/com/example/demo/DiplomaApplication.java
+++ src/main/java/com/example/demo/DiplomaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class DiplomaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaApplication.class, args);
+    }
 
 }
