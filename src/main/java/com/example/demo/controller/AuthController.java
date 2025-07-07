853185eae9b87e75a7633424e674c774eee7666a
YuraEnder <yuraender@yandex.ru>
2025-07-07 07:48:33 +0300

    Custom error page

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +13,5 @@ public class AuthController {
             model.addAttribute("error", "Неправильные имя пользователя или пароль.");
         }
         return "login";
-    }
-
-    @GetMapping("/default")
-    public String defaultAfterLogin(HttpSession session) {
-        String role = (String) session.getAttribute("role");
-        if (role.equals("administrator")) {
-            session.setAttribute("isAdmin", true);
-            return "redirect:/home";
-        }
-        return "redirect:/home";
     }
 }
